daa90d95e2045e9404437f9e7dcdd11d3c6293fe
szymonzaczek <31451720+szymonzaczek@users.noreply.github.com>
2020-07-15 09:54:19 +0200

    Removing redundant import
    Removing redundant import

--- twitter_sentiment_visualisation_pipeline/twitter_sentiment_visualisation_pipeline.py
+++ twitter_sentiment_visualisation_pipeline/twitter_sentiment_visualisation_pipeline.py
@@ -3,7 +3,6 @@ import os
 import datetime
 import nltk
 import re
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
